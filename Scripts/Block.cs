46844d49ac634a4218d2570811033d173f06c2a7
Kevin Tyther <90258258+FishyFrenz@users.noreply.github.com>
2022-04-12 08:38:21 -0400

    Block

--- Scripts/Block.cs
+++ Scripts/Block.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    Level lvl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lvl = FindObjectOfType<Level>();
+        lvl.CountBlock();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Destroy(gameObject, 0f); // 0f = delay
+        lvl.BlockDestoryed();
     }
 }
